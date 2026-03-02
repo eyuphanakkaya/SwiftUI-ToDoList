fe5ef79a03639c847cec2a147abd5ffd2feadd1b
eyuphanakkaya <akkayaeyupp@gmail.com>
2026-03-02 12:06:52 +0300

    Create a 'User Defaults Manager' to manage 'Persistent' processes within the application.

---
+++ ToDoList/Managers/UserDefaultsManager.swift
@@ -0,0 +1,47 @@
+//
+//  UserDefaultsManager.swift
+//  ToDoList
+//
+//  Created by Eyüphan Akkaya on 2.03.2026.
+//
+
+import Foundation
+
+protocol Persisting {
+    associatedtype Item: Codable
+    
+    func save(items: Item)
+    func load() -> Item?
+    func delete()
+}
+
+final class UserDefaultsManager<T: Codable>: Persisting {
+    typealias Item = T
+    
+    private let defaults: UserDefaults
+    private let key: String = "userDefaultsKey"
+    
+    init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+    }
+    
+    func save(items: T) {
+        let encoder = JSONEncoder()
+        let encoded = try? encoder.encode(items)
+        defaults.set(encoded, forKey: key)
+    }
+    
+    func load() -> T? {
+        guard let data = UserDefaults.standard.data(forKey: key),
+              let decoded = try? JSONDecoder().decode(T.self, from: data) else {
+            return nil
+        }
+        
+        return decoded
+    }
+    
+    func delete() {
+        defaults.removeObject(forKey: key)
+    }
+    
+}
